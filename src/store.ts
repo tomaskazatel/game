a0c88fb5fcd36cd50526457a9c2e643c6a5156a1
Tom Kazatel <kazatel@jiktogroup.cz>
2019-04-18 14:56:10 +0200

    Add data preparation

--- src/store.ts
+++ src/store.ts
@@ -1,22 +1,40 @@
 import Shuffle from './utils/shuffle';
-import items from './data.json';
 
 export interface Item {
     key: number;
     value: string;
-    isImage: boolean;
     match: number;
 }
 
 class Store {
-    private shuffledItems: Item[] = Shuffle(items);
+    private jsItems: string[] = ['ReactJS', 'VueJS', 'AngularJS', 'EmberJS', 'MeteorJS'];
+    private preparedItems: Item[] = [];
+    private shuffledItems: Item[] = [];
+
+    constructor() {
+        this.prepareData();
+        this.shuffledItems = Shuffle(this.preparedItems);
+    }
 
     get items(): Item[] {
         return this.shuffledItems;
     }
 
     public restart() {
-        this.shuffledItems = Shuffle(items);
+        this.shuffledItems = Shuffle(this.preparedItems);
+    }
+
+    private prepareData(): void {
+        this.jsItems.forEach((jsItem: string, key: number) => {
+            [0, 1].forEach((increase: number) => {
+                const tempKey: number = key * 2;
+                this.preparedItems.push({
+                    key: tempKey + increase,
+                    value: jsItem,
+                    match: increase ? tempKey : tempKey + 1,
+                });
+            });
+        });
     }
 }
 
